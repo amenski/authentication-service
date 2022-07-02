22a9b40012bb00b4b64171ae89fe577b113120f6
amenski <amantwd@gmail.com>
2022-07-02 18:42:42 +0300

    add endpoint ddl

--- src/main/resources/db/dml.sql
+++ src/main/resources/db/dml.sql
@@ -1,3 +1,7 @@
 -- auth_roles
 INSERT INTO `auth_role` VALUES (1,'ADMIN', 'ADMIN');
 INSERT INTO `auth_role` VALUES (2,'USER', 'USER');
+
+INSERT INTO auth_endpoint (service_name,endpoint,http_method,permission) VALUES ('employee-service','/v1/create','POST','POST_EMPLOYEE');
+INSERT INTO auth_endpoint (service_name,endpoint,http_method,permission) values ('employee-service','/v1/{employee-id}','GET','GET_EMPLOYEE');
+INSERT INTO auth_endpoint (service_name,endpoint,http_method,permission) values ('employee-service','/v1/{employee-id}','PUT','UPD_EMPLOYEE');
